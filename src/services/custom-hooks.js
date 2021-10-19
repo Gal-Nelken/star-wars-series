fa2131ceb0083f443459947a70c70af7b3b22bea
Gal-Nelken <galnelken07@gmail.com>
2021-10-19 18:52:21 +0300

    V 1.01

--- src/services/custom-hooks.js
+++ src/services/custom-hooks.js
@@ -7,10 +7,16 @@ export const useCheckInArr = (entity, arr) => {
     const [isInArr, setIsFav] = useState(false)
 
     const checkArr = (id) => {
-        // if (!id) return
-        const isExsist = arr.some(entity => entity._id === id)
-        setIsFav(isExsist)
+        try{
+
+            if (!entity || !id) return
+            const isExsist = arr.some(entity => entity._id === id)
+            setIsFav(isExsist)
+        } catch (err){
+            console.error('HAD A PROBLEM\n', err)
+        }
     }
+
 // CHECK EVREY TIME THE ARRAY OR ENTITY CHANGE
     useEffect(() => {
         checkArr(entity._id)
